584a4a9f3b8507c848bcf407ca1cdd5174d892eb
Per Nielsen <pernielsen1@outlook.com>
2025-03-19 19:41:01 +0100

    back on other computer - all activated

--- build_common.sh
+++ build_common.sh
@@ -2,11 +2,16 @@
 # https://pkiage.hashnode.dev/creating-a-local-python-package-in-a-virtual-environment
 # file setup.py and __init__.py are there but small
 # https://earthly.dev/blog/create-python-package/
-TARGET_COMMON="common"
-mkdir $TARGET_COMMON
-python3 -m venv $TARGET_COMMON
-source $TARGET_COMMON/bin/activate
-cd $TARGET_COMMON
+TARGET_DIR="common"
+mkdir $TARGET_DIR
+rm -rf $TARGET_DIR/local_packages
+rm -rf $TARGET_DIR/bin
+rm -rf $TARGET_DIR/include
+rm -rf $TARGET_DIR/lib
+rm $TARGET_DIR/lib64
+python3 -m venv $TARGET_DIR
+source $TARGET_DIR/bin/activate
+cd $TARGET_DIR
 pip install -r ../common_requirements.txt
 # now build the pn_utilities package
 python3 setup.py sdist
--- clients/build_restapi_venv.sh
+++ clients/build_restapi_venv.sh
@@ -2,6 +2,12 @@
 # builds venv for a python client
 TARGET_DIR="python_restapi"
 mkdir  $TARGET_DIR
+rm -rf $TARGET_DIR/local_packages
+rm -rf $TARGET_DIR/bin
+rm -rf $TARGET_DIR/include
+rm -rf $TARGET_DIR/lib
+rm $TARGET_DIR/lib64
+
 python3 -m venv $TARGET_DIR
 source $TARGET_DIR/bin/activate
 cd $TARGET_DIR	
@@ -12,3 +18,4 @@ pip install -r ../python_restapi_requirements.txt
 pip install ../../common/dist/pn_utilities-0.1.tar.gz
 # now in vscode just select the python3.12 found in python_mysql/bin/....
 cd ..
+# should we update the requirements file Now ?...
