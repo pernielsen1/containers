8b92d59313a5e5b915c732280e67b4bb425c20cf
per.nielsen1@outlook.com <per.nielsen1@outlook.com>
2025-04-07 17:21:39 +0200

    fixed git ignore

--- clients/python_restapi/lib/python3.12/site-packages/pn_utilities/__init__.py
+++
@@ -1,7 +0,0 @@
-# https://www.geeksforgeeks.org/what-is-__init__-py-file-in-python/
-# Define the __all__ variable
-__all__ = ["PnLogger"]
-#__all__ = ["module1", "module2"]
-
-# Import the submodules
-from . import PnLogger
