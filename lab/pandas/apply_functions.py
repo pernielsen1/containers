d4d6c4c446ccabf09f51c7754ac30052e795ad3c
Per Nielsen <per.nielsen1@outlook.com>
2026-03-30 19:33:57 +0200

    more functions

--- lab/pandas/apply_functions.py
+++ lab/pandas/apply_functions.py
@@ -4,12 +4,21 @@ def add_two_columns(row):
     result = [row['x'] * 2, row['y']*3]
     return result
 
+def add_dict(row):
+    res={}
+    res['a'] = 2 * row['x']
+    res['b'] = 7 * row['x']
+    return pd.Series(res)
+
 def do_it():
     df = pd.DataFrame({"x": [1,2,3],"y":[5,6,7]})
     print(df)
     df[['z', 'w']] = df.apply(add_two_columns, axis=1, result_type='expand')
     print(df)
- 
+    df[['xx', 'xxx']] = df.apply(add_dict, axis=1, result_type='expand')
+    print(df)
+
+
 #--------------------------------
 if __name__ == '__main__':
     do_it()
