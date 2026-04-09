01608111f7d32f4bb123388eb0adbadd871b4fa1
Per Nielsen <per.nielsen1@outlook.com>
2026-04-09 20:15:35 +0200

    containers

---
+++ claude_exp/company_id_sql/build_container.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker compose build --build-arg PN_MYSQL_USER=pnx --build-arg PN_MYSQL_PASSWORD=$PN_MYSQL_PASSWORD
---
+++ claude_exp/company_id_sql/start_container.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo docker run -i -t company_id_sql-company_id_container:latest
