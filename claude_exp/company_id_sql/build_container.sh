5ef2a894a6f584438275310348c7eca44aa12f89
Per Nielsen <per.nielsen1@outlook.com>
2026-04-09 20:44:45 +0200

    updated container with environment variables

--- claude_exp/company_id_sql/build_container.sh
+++ claude_exp/company_id_sql/build_container.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker compose build --build-arg PN_MYSQL_USER=pnx --build-arg PN_MYSQL_PASSWORD=$PN_MYSQL_PASSWORD
+docker compose build 
+# -build-arg PN_MYSQL_USER=pnx --build-arg PN_MYSQL_PASSWORD=$PN_MYSQL_PASSWORD
